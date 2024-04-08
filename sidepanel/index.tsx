83fc452342f7883621242545ff726f1a021baf4b
Bruce Allan Makaaru <bmakaaru@canteenlabs.com>
2024-04-08 22:50:34 +0300

    Setup theme

--- sidepanel/index.tsx
+++ sidepanel/index.tsx
@@ -8,28 +8,27 @@ function IndexSidePanel() {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16,
-      }}
-    >
-      <img src={logo} alt="Some pretty cool image" />
+    <div className="flex flex-col gap-5 p-5">
+      <img src={logo} alt="Some pretty cool image" className="w-24" />
 
-      <FaBeer className="text-6xl text-red-300" />
-      <h2>
-        Welcome to your
-        <a href="https://www.plasmo.com" target="_blank">
-          {" "}
-          Plasmo 123
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+      <FaBeer className="text-2xl text-red-300" />
+      <h1>Welcome to your extension</h1>
+      <h2>Welcome to your extension</h2>
+      <h3>Welcome to your extension</h3>
+      <h4>Welcome to your extension</h4>
+      <h5>Welcome to your extension</h5>
+      <h6>Welcome to your extension</h6>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.
+        <a href="https://docs.plasmo.com" target="_blank">
+          View Docs
+        </a>
+      </p>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   mode: "jit",
   darkMode: "class",
   content: ["./**/*.tsx"],
-  plugins: []
+  plugins: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['"Nunito"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
 }
