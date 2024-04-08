61639a72578bbac9ee83cca7e7b22ebdd14a72a8
Bruce Allan Makaaru <bmakaaru@canteenlabs.com>
2024-04-09 00:50:56 +0300

    Read from supabase db

--- sidepanel/index.tsx
+++ sidepanel/index.tsx
@@ -1,47 +1,38 @@
 import logo from "data-base64:~assets/logo.svg"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { FaBeer } from "react-icons/fa"
 
 import "../style.css"
 
 import { Button } from "~components/ui/button"
+import { supabase } from "~core/supabase"
 
 function IndexSidePanel() {
-  const [data, setData] = useState("")
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const getURATaxes = async () => {
+      const res = await supabase.from("ura_taxes").select("*")
+      if (res.error) {
+        console.error(res.error)
+      } else {
+        setData(res.data)
+      }
+    }
+
+    getURATaxes()
+  }, [])
 
   return (
-    <div className="flex flex-col gap-5 p-5">
-      <img src={logo} alt="Some pretty cool image" className="w-24" />
-
-      <Button size="sm" variant="default">
-        Hello
-      </Button>
-      <Button size="lg" variant="secondary">
-        Hello
-      </Button>
-      <Button variant="outline">Hello</Button>
-      <Button variant="destructive">Hello</Button>
-      <Button variant="ghost">Hello</Button>
-      <Button variant="link">Hello</Button>
-
-      <FaBeer className="text-2xl text-red-300" />
-      <h1>Welcome to your extension</h1>
-      <h2>Welcome to your extension</h2>
-      <h3>Welcome to your extension</h3>
-      <h4>Welcome to your extension</h4>
-      <h5>Welcome to your extension</h5>
-      <h6>Welcome to your extension</h6>
-
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum.
-        <a href="https://docs.plasmo.com" target="_blank">
-          View Docs
-        </a>
-      </p>
+    <div className="p-5">
+      {data?.map((row) => (
+        <div key={row.id} className="flex flex-col gap-5">
+          <p>{row.modal_code}</p>
+          <p>{row.registration_year}</p>
+          <p>{row.engine_size}</p>
+          <p>{row.usd}</p>
+        </div>
+      ))}
     </div>
   )
 }
