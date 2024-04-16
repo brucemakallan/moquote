c9116f2fdb76e11a38aa9ee728c8354f2ec33f15
Bruce Allan Makaaru <bmakaaru@canteenlabs.com>
2024-04-16 21:28:52 +0300

    Fix missing env variables and send email via supabase edge-function

--- supabase/functions/send_quote_request_email/index.ts
+++ supabase/functions/send_quote_request_email/index.ts
@@ -1,28 +1,61 @@
-const RESEND_API_KEY = Deno.env.get('RESEND_API_KEY')
+const RESEND_API_KEY = Deno.env.get('RESEND_API_KEY');
 
-const handler = async (_request: Request): Promise<Response> => {
-  const res = await fetch('https://api.resend.com/emails', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${RESEND_API_KEY}`,
-    },
-    body: JSON.stringify({
-      from: 'MoQuote <onboarding@resend.dev>',
-      to: 'brucemakallan@gmail.com',
-      subject: 'A MoQuote user has requested a quote',
-      html: '<strong>Please send me a quotation for this ...</strong>',
-    }),
-  })
+const handler = async (request: Request): Promise<Response> => {
+  try {
+    const req = await request.json();
+    console.log(`req, ${JSON.stringify(req)}`);
 
-  const data = await res.json()
+    if (!RESEND_API_KEY) throw new Error('RESEND_API_KEY not set');
 
-  return new Response(JSON.stringify(data), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
+    if (!req?.record?.id) throw new Error('Record not found');
+
+    const { record } = req;
+    const html = `
+    <strong>Please send me a quotation for this ...</strong>
+    Email: ${record.email}
+    URL: ${record.url}
+    Image url: ${record.image_url}
+    Heading: ${record.heading}
+    Year: ${record.year}
+    Capacity: ${record.capacity}
+    Model: ${record.model}
+    Price: ${record.price}
+    Tax: ${record.tax}
+    Ugx rate: ${record.ugx_rate}
+    Requested at: ${record.created_at}
+    `;
+
+    const res = await fetch('https://api.resend.com/emails', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${RESEND_API_KEY}`,
+      },
+      body: JSON.stringify({
+        from: 'MoQuote <onboarding@resend.dev>',
+        to: 'brucemakallan@gmail.com',
+        subject: 'A MoQuote user has requested a quotation',
+        html,
+      }),
+    });
+
+    const data = await res.json();
+    console.log(`response, ${JSON.stringify(data)}`);
+
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    console.error(`Error processing request: ${error.message}`);
+
+    return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
 }
 
-Deno.serve(handler)
+Deno.serve(handler);
